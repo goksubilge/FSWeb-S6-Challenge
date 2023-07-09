5af59b2031b64e2d67ac3e715f3020e5ec69a5c7
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-10 01:07:52 +0300

    add karakter.js & justify karakter main key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import Karakter from './components/Karakter';
 
 const App = () => {
   const [karakters, setKarakters] = useState([]);
@@ -29,6 +30,22 @@ useEffect (() => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+      {loading && <p>........LOADING.......</p>}
+      <h1 className="Header">Characters</h1>
+      {karakters.length > 0 && karakters.map((character) => {return <Karakter character={character} />
+      })}
+      {error && <p>!ERROR! : {error.message}{error.code} </p> }
+    </div>
+  );
+}
+
+export default App;
+
   /* karakterleri setKarakters(response.data) ile axiosa tanıttım, {karakters.map((character) => {return <div>{character.name}</div>})}
   ile çağırdım ve display */
   /* sonra loaing ve error ları tanımladım. axiosa da tanıttım. Aşağıya gittim loaing true ise bana loading yazısını göster dedim 
@@ -38,17 +55,5 @@ useEffect (() => {
 /* axios.then kısmında (data) ya eriştiyse internet, loading im false olcak(loaded olmuş oluyor yani), setLoading(false) de ekledim*/
 /* şuan hata varsa da görmüyorum. gittim consoleda inceledim. axios.catch te setError(error) yazıp aşağıya da {error.message}{error.code} 
 v aynı zamanda hata varsa loadingimin de bitmiş olması lazım o yüzden axis.catch e setLoading(false) ekledim. */
-
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-      {loading && <p>........LOADING.......</p>}
-      <h1 className="Header">Characters</h1>
-      {karakters.length > 0 && karakters.map((character) => {return <div>{character.name}</div>
-      })}
-      {error && <p>!ERROR! : {error.message}{error.code} </p> }
-    </div>
-  );
-}
-
-export default App;
+/* gidip karakter.js ini oluşturdum ve proplarını tanımladım. bu sayfaya  import ettikten sonra 
+karakters.map((character) => {return <div>{character.name}</div>} return kısmını {return <Karakter character={character} /> olarak güncelledim */
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,13 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react";
+
+const Karakter = (props) => {
+ const {character} = props;
+ return <div> {character.name} </div>
+}
+
+export default Karakter;
+
+// fonks çalıştığında bir html div dönüyorsa o fonksiyona bileşen diyoruz. bunu gidip app.js e import ediyorum.
+// {karakters.map((character) => {return <div>{character.name}</div>} satırını
+//{karakters.length > 0 && karakters.map((character) => {return <Karakter character={character}/> satırı yaptım.
