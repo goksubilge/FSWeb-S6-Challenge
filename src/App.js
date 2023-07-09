c1eb27faf1e5557d110687d7be0ca10e19f499ad
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-10 00:44:21 +0300

    add loading & error

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+
 const App = () => {
   const [karakters, setKarakters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
 useEffect (() => {
   axios.get('https://swapi.dev/api/people/')
   .then(function (response) {
     // handle success
-    console.log(response);
+    console.log(response.data);
+    setLoading(false);
+    setError(false);
     setKarakters(response.data);
   })
   .catch(function (error) {
     // handle error
+    setLoading(false);
+    setError(error);
     console.log(error);
   })
   .finally(function () {
     // always executed
   });
-}, [])
+},[])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  /* karakterleri setKarakters(response.data) ile axiosa tanıttım, {karakters.map((character) => {return <div>{character.name}</div>})}
+  ile çağırdım ve display */
+  /* sonra loaing ve error ları tanımladım. axiosa da tanıttım. Aşağıya gittim loaing true ise bana loading yazısını göster dedim 
+  şu şekilde => süslü parantez loading && <p>........LOADING.......</p> süslü parantez */
+/* !loaing tamasa yani characters.length=o ise && => yazdır diyorum karakterler isimlerini ekrana tek tek listele*/
+// error && <p>!ERROR!</p> ekledim. 
+/* axios.then kısmında (data) ya eriştiyse internet, loading im false olcak(loaded olmuş oluyor yani), setLoading(false) de ekledim*/
+/* şuan hata varsa da görmüyorum. gittim consoleda inceledim. axios.catch te setError(error) yazıp aşağıya da {error.message}{error.code} 
+v aynı zamanda hata varsa loadingimin de bitmiş olması lazım o yüzden axis.catch e setLoading(false) ekledim. */
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
-      {karakters.map((character) => {return <div>{character.name}</div>
+      <h1 className="Header">React Wars</h1>
+      {loading && <p>........LOADING.......</p>}
+      <h1 className="Header">Characters</h1>
+      {karakters.length > 0 && karakters.map((character) => {return <div>{character.name}</div>
       })}
+      {error && <p>!ERROR! : {error.message}{error.code} </p> }
     </div>
   );
 }
