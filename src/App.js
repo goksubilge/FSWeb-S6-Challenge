811ed88cf82b2c9be93ebf6f60eee6e19087161a
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-09 23:25:42 +0300

    update app.css + index.css (add new image)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
-
+import { useState, useEffect } from 'react';
 const App = () => {
-  axios.get('https://swapi.dev/api/people/?page=2')
+  const [karakters, setKarakters] = useState([]);
+
+useEffect (() => {
+  axios.get('https://swapi.dev/api/people/')
   .then(function (response) {
     // handle success
     console.log(response);
+    setKarakters(response.data);
   })
   .catch(function (error) {
     // handle error
@@ -13,6 +17,7 @@ const App = () => {
   .finally(function () {
     // always executed
   });
+}, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -21,6 +26,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {karakters.map((character) => {return <div>{character.name}</div>
+      })}
     </div>
   );
 }
