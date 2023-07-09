6c73bddc15994ec7b6ed8995abdaffc5834e0e19
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-09 20:33:42 +0300

    axios test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  axios.get('https://swapi.dev/api/people/?page=2')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
